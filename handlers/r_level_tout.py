f7a236c53451fa836ea964d8e17c2ac1290dcf54
Igor Dubov <4dubov@gmail.com>
2024-06-25 11:43:29 +0300

    fix

--- handlers/r_level_tout.py
+++ handlers/r_level_tout.py
@@ -114,7 +114,7 @@ def r_level_timeout_v2(vars):
     #     vars.v1 = vars.result_in
     #     vars.result = (vars.v1 + vars.v2 + vars.v3 + vars.v4 + vars.v5 + vars.v6 + vars.v7 + vars.v8 + vars.v9 + vars.v10)/10
 # !!!! ------------- dev-------------------        
-        vars.result = vars.result_in
+        # vars.result = vars.result_in
 # !!!! ------------- dev-------------------        
         result = vars.result
         if result <= vars.gr_stand:  # откл

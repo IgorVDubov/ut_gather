1ee7925ab7ab5c6c93d5b64bf30b5bc833d81b7e
Igor Dubov <4dubov@gmail.com>
2025-11-13 18:24:38 +0300

    ready gc 0.8

--- main.py
+++ main.py
@@ -1,65 +1,62 @@
 #!/usr/bin/env python
-from gathercore.app import app_builder
+import sys
 
+from gathercore.app import app_builder
 from gathercore.mylib import logger as loggerLib
 from loguru import logger
 
-import sys
-if sys.platform == 'win32':                 # Если запускаем из под win
+if sys.platform == "win32":  # Если запускаем из под win
     import asyncio
+
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 sys.path.append("/gathercore")
 
-import config
-import settings
-import scadaconfig as scada_config
 from gathercore.interfaces.db import create_db_interface
+
+import config
+import scadaconfig as scada_config
+import settings
 import web.handlers as project_webserver_handlers
 from init import init_args
 
-# project_init_func - функция инициализации проекта, 
+# project_init_func - функция инициализации проекта,
 # которая запускается после формирования
-# базы каналов и databus перед стартом основного цикла, 
+# базы каналов и databus перед стартом основного цикла,
 # параметром в функцию передается databus
 
 try:
     from init import init as project_init_func
-except (ModuleNotFoundError | ImportError):
+except ModuleNotFoundError | ImportError:
     project_init_func = None
 
+
 def main():
-    loggerLib.loggerInit('DEBUG', 'error', ('PROG',))
-    logger.info('Starting........')
+    loggerLib.loggerInit("DEBUG", "error", ("PROG",))
+    logger.info("Starting........")
     # создаем интерфейс к базе данных
-    db_interface = create_db_interface('db_interface',
-                                       config.DB_TYPE,
-                                       config.DB_PARAMS
-                                       )
+    db_interface = create_db_interface("db_interface", config.DB_TYPE, config.DB_PARAMS)
     # создаем приложение
     app = app_builder(
-                source_list=scada_config.source_list,
-                channels_config=scada_config.channels_config,
-                mb_server_addr_map=scada_config.mb_server_addr_map,
-                modbus_server_params=config.modbus_server_params,
-                project_webserver_params=config.http_server_params,
-                project_webserver_handlers=project_webserver_handlers.handlers,
-                project_web_users=settings.users,
-                project_init_func=project_init_func,
-                databus_objects=[db_interface]
-                )
+        interfaces=scada_config.interfaces,
+        source_list=scada_config.source_list,
+        channels_config=scada_config.channels_config,
+        modbus_server_params=config.modbus_server_params,
+        mb_server_addr_map=scada_config.mb_server_addr_map,
+        project_webserver_params=config.http_server_params,
+        project_webserver_handlers=project_webserver_handlers.handlers,
+        project_web_users=settings.users,
+        project_init_func=project_init_func,
+        databus_objects=[db_interface],
+    )
     # добавляем словарь websocket клиентов для панелей оператора на станках
     # {machine_id: web_socket_client}
-    app.databus.add_object('machine_WS_client', dict()) 
+    app.databus.add_object("machine_WS_client", dict())
     # Инициализация необходимых аргументов через БД settings
-    init_args(
-        app.databus.get_object('db_interface'),
-        app.channel_base
-        )
+    init_args(app.databus.get_object("db_interface"), app.channel_base)
     # запуск app
     app.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
     # test_component()
-

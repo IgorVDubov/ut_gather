2aa1e434ed662c3871daa9400899963da1b6578c
Igor Dubov <4dubov@gmail.com>
2026-03-18 10:36:30 +0300

    add prj

--- handlers/dbwriter.py
+++ handlers/dbwriter.py
@@ -1,23 +1,22 @@
-
 def db_writer(vars):
-    '''
-        принудительная запись в БД
-        'scheduller_write_states': '13001.args.write_init', # оигнал записи статусов (через 3 сек после записи счетчиков) 
-        'scheduller_write_counters': '13002.args.write_init',   # оигнал записи счетчиков
-        'mb_write_init': None,                         # сюда значение из функции записи регистра MBExchangeServer 
-        'mb_write_init_reset': '2999.result_in',       # пишем значение в регистр собственного MBExchangeServer
-        'mb_write_runs_flag': False,                   # идет процесс записи от сигнала mb - для исключения двойной записи пока сбрасывакется сигнал
-        'write_init_2120': False,
-        'write_counter_2120': False,
-        'write_init_2040': False,
-        'write_counter_2040': False,
-    '''
+    """
+    принудительная запись в БД
+    'scheduller_write_states': '13001.args.write_init', # оигнал записи статусов (через 3 сек после записи счетчиков)
+    'scheduller_write_counters': '13002.args.write_init',   # оигнал записи счетчиков
+    'mb_write_init': None,                         # сюда значение из функции записи регистра MBExchangeServer
+    'mb_write_init_reset': '2999.result_in',       # пишем значение в регистр собственного MBExchangeServer
+    'mb_write_runs_flag': False,                   # идет процесс записи от сигнала mb - для исключения двойной записи пока сбрасывакется сигнал
+    'write_init_2120': False,
+    'write_counter_2120': False,
+    'write_init_2040': False,
+    'write_counter_2040': False,
+    """
     if vars.scheduller_write_states:
-        print('!!!!!!!!!!    sheduller write states  !!!!!!')
+        print("!!!!!!!!!!    sheduller write states  !!!!!!")
     if vars.scheduller_write_counters:
-        print('!!!!!!!!!!    sheduller write counters   !!!!!!')
+        print("!!!!!!!!!!    sheduller write counters   !!!!!!")
     if vars.mb_write_init:
-        print('!!!!!!!!!!    modbus signal write_init   !!!!!!')
+        print("!!!!!!!!!!    modbus signal write_init   !!!!!!")
 
     if vars.mb_write_init is False and vars.mb_write_init_reset is False:
         vars.mb_write_init_reset = None
@@ -26,21 +25,25 @@ def db_writer(vars):
     if vars.scheduller_write_states:
         vars.write_init_2120 = True
         vars.write_init_2040 = True
+        vars.write_init_2100 = True
         vars.write_init_1416 = True
         vars.write_init_1501 = True
         vars.write_init_2901 = True
         vars.write_init_2902 = True
-        
+
     if vars.scheduller_write_counters:
         vars.write_counter_2120 = True
         vars.write_counter_2040 = True
-    
+        vars.write_counter_2100 = True
+
     if vars.mb_write_init and not vars.mb_write_runs_flag:
         vars.mb_write_runs_flag = True
         vars.write_init_2120 = True
         vars.write_counter_2120 = True
         vars.write_init_2040 = True
         vars.write_counter_2040 = True
+        vars.write_init_2100 = True
+        vars.write_counter_2100 = True
         vars.write_init_1416 = True
         vars.write_init_1501 = True
         vars.write_init_2901 = True
@@ -48,9 +51,9 @@ def db_writer(vars):
 
     if vars.scheduller_write_states:
         vars.scheduller_write_states = False
-    
+
     if vars.scheduller_write_counters:
         vars.scheduller_write_counters = False
-    
+
     if vars.mb_write_init:
         vars.mb_write_init_reset = False
